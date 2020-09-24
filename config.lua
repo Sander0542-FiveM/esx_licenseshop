c046a9b913cde39a81a057cb4f8049e0a90579ab
Sander Jochems <sanderjochems@hotmail.nl>
2020-09-24 14:10:43 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,14 +7,14 @@ Config.DrawDistance = 10
 Config.MarkerInfo = {Type = 1, r = 102, g = 102, b = 204, x = 2.0, y = 2.0, z = 1.0}
 Config.BlipLicenseShop = {Sprite = 408, Color = 0, Display = 2, Scale = 1.0}
 
-Config.UseBlips = false -- true = Use License Shop Blips
-Config.RequireDMV = false -- If true then it will require players to have Drivers Permit to buy other Licenses | false does the Opposite.
+Config.UseBlips = true -- true = Use License Shop Blips
+Config.RequireDMV = true -- If true then it will require players to have Drivers Permit to buy other Licenses | false does the Opposite.
 
-Config.AdvancedVehicleShop = false -- Set to true if using esx_advancedvehicleshop
+Config.AdvancedVehicleShop = true -- Set to true if using esx_advancedvehicleshop
 Config.LicenseAircraft = 10000
 Config.LicenseBoating = 50
 
-Config.AdvancedWeaponShop = false -- Set to true if using esx_advancedweaponshop
+Config.AdvancedWeaponShop = true -- Set to true if using esx_advancedweaponshop
 Config.LicenseMelee = 1
 Config.LicenseHandgun = 10
 Config.LicenseSMG = 100
@@ -23,7 +23,7 @@ Config.LicenseAssault = 250
 Config.LicenseLMG = 1000
 Config.LicenseSniper = 1500
 
-Config.DMVSchool = false -- Set to true if using esx_dmvschool
+Config.DMVSchool = true -- Set to true if using esx_dmvschool
 Config.SellDMV = false -- Set to true if Config.RequireDMV = false & you want players to be able to buy Drivers Permit
 Config.LicenseCommercial = 300
 Config.LicenseDrivers = 150
